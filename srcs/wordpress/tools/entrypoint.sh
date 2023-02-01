0f203766f4d48a8ad93db7fa74b9ebcb3cd97e30
enaenen <enaenen@naver.com>
2023-02-01 21:58:42 +0900

    s

---
+++ srcs/wordpress/tools/entrypoint.sh
@@ -0,0 +1,25 @@
+# https://make.wordpress.org/cli/handbook/guides/installing/
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www
+wp core download --allow-root --path=/var/www
+
+cp /tmp/wp-config.php /var/www
+
+wp core install --allow-root --path=/var/www \
+								--url=wchae.42.fr \
+								--title=Inception \
+								--admin_user=wchae \
+								--admin_password=4242 \
+								--admin_email=wchae@student.42seoul.kr \
+								--skip-email
+
+wp user create \
+							wchae wchae@student.forest.kr \
+							--allow-root --path=/var/www \
+							--role=author \
+							--user_pass=4242
+
+exec php-fpm8 -F
