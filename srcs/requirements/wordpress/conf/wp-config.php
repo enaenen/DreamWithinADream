0059ae51707286db58dc3e6a0c34b0c65ff433ee
enaenen <enaenen@naver.com>
2023-02-06 22:38:59 +0900

    compose

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', getenv("DB_NAME") );
 
 /** Database username */
-define( 'DB_USER', getenv("USER_ID") );
+define( 'DB_USER', getenv("DB_USER") );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv("USER_PASSWORD") );
+define( 'DB_PASSWORD', getenv("DB_PASSWORD") );
 
 /** Database hostname */
 define( 'DB_HOST', getenv("DB_HOST") );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         getenv("WORDPRESS_AUTH_KEY") );
+define( 'SECURE_AUTH_KEY',  getenv("WORDPRESS_SECURE_AUTH_KEY") );
+define( 'LOGGED_IN_KEY',    getenv("WORDPRESS_LOGGED_IN_KEY") );
+define( 'NONCE_KEY',        getenv("WORDPRESS_NONCE_KEY") );
+define( 'AUTH_SALT',        getenv("WORDPRESS_AUTH_SALT") );
+define( 'SECURE_AUTH_SALT', getenv("WORDPRESS_SECURE_AUTH_SALT") );
+define( 'LOGGED_IN_SALT',   getenv("WORDPRESS_LOGGED_IN_SALT") );
+define( 'NONCE_SALT',       getenv("WORDPRESS_NONCE_SALT") );
 
 /**#@-*/
 
