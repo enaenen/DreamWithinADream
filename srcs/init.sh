a86611ec298cf60dfd2ff282211fd6e505eefb26
enaenen <enaenen@naver.com>
2023-02-08 00:20:29 +0900

    ubuntu init script

---
+++ srcs/init.sh
@@ -0,0 +1,29 @@
+sudo apt-get update
+
+sudo apt-get install \
+    curl \
+    gnupg \
+    ca-certificates \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# docker repository add
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt update
+sudo apt-get update -y
+
+# install docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# docker group add
+sudo groupadd docker
+sudo usermod -aG docker wchae
+
+echo '127.0.0.1 wchae.42.kr' | sudo tee -a /etc/hosts
+
+sudo reboot
